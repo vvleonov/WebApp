8eea1a29d1442babccd33745bc1292b4d56f585c
Sensei_28777 <lww28777@yandex.ru>
2022-04-10 21:40:30 +0300

    Added some tests

--- App/__init__.py
+++ App/__init__.py
@@ -30,4 +30,8 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
+    @app.route('/hello')
+    def hello():
+        return 'Hi'
+
     return app
---
+++ tests/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (username, password)
+VALUES
+    ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
+    ('other', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f');
+
+INSERT INTO post (title, body, author_id, created)
+VALUES
+    ('test title', 'test' || x'0a' || 'body', 1, '2022-01-01 00:00:00');
